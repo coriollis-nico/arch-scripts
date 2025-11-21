29c0915ceb6869e3a71d8d41bbcc37fd394ef9e3
Nicolás Celedón-Hernández <coriollis.nch@gmail.com>
2025-11-20 20:59:20 -0600

    First commit

---
+++ kde.sh
@@ -0,0 +1,16 @@
+cat kde_main.txt | sudo pacman -Syu --needed -
+
+sudo systemctl enable sddm.service
+
+
+cat maintenance.txt | sudo pacman -S --needed -
+
+sudo systemctl enable bluetooth.service
+sudo systemctl enable cups.socket
+sudo systemctl enable power-profiles-daemon.service
+sudo systemctl enable thermald.service
+
+
+cat apps.txt | sudo pacman -S --needed -
+
+cat screenshare.txt | sudo pacman -S --needed -
